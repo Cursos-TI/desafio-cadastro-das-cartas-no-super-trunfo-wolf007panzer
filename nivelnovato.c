5736314f1b7d821c422cbecfed681037ca6d7dda
wolf007panzer <randleysantos1@gmail.com>
2025-03-21 19:57:39 +0000

    correcoes no nivelnovato

--- nivelnovato.c
+++ nivelnovato.c
@@ -8,7 +8,7 @@ printf("commit\n");
 char estado[20];
 char codigo [20];
 char nomedacidade [20];
-int populaçao;
+int populacao;
 float area;
 float PIB;
 int numerodepontosturisticos;
@@ -17,16 +17,16 @@ int numerodepontosturisticos;
 strcpy(estado, "Minas Gerais");
 strcpy(codigo, "MG");
 strcpy(nomedacidade, "Belo Horizonte");
-populaçao = 2523794;
-area = 331.401;
+populacao = 2.315560;
+area = 331.4;
 PIB = 140.1;
 numerodepontosturisticos = 10;
 
 printf("Estado: %s\n", estado);
 printf("Codigo: %s\n", codigo);
 printf("Nome da cidade: %s\n", nomedacidade);
-printf("População: %d\n", populaçao);
-printf("Area: %.2f\n", area);
+printf("População: %d\n", populacao);
+printf("Area: %.1f\n", area);
 printf("PIB: %.f\n", PIB);
 printf("Número de pontos turisticos: %d\n", numerodepontosturisticos);
 
@@ -34,7 +34,7 @@ printf("Número de pontos turisticos: %d\n", numerodepontosturisticos);
 char estado2[20];
 char codigo2 [20];
 char nomedacidade2 [20];
-int populaçao2;
+int populacao2;
 float area2;
 float PIB2;
 int numerodepontosturisticos2;
@@ -43,74 +43,60 @@ int numerodepontosturisticos2;
 strcpy(estado2, "São Paulo");
 strcpy(codigo2, "SP");
 strcpy(nomedacidade2, "São Paulo");
-populaçao2 = 12106920;
-area2 = 1521.11;
+populacao2 = 11.895578;
+area2 = 1.521;
 PIB2 = 699.6;
 numerodepontosturisticos2 = 10;
 
 printf("Estado: %s\n", estado2);
 printf("Codigo: %s\n", codigo2);
 printf("Nome da cidade: %s\n", nomedacidade2);
-printf("População: %d\n", populaçao2);
-printf("Area: %.2f\n", area2);
+printf("População: %d\n", populacao2);
+printf("Area: %.3f\n", area2);
 printf("PIB: %.f\n", PIB2);
 printf("Número de pontos turisticos: %d\n", numerodepontosturisticos2);
 
 printf("Digite o estado: ");
 scanf ("%s", estado); 
-printf ( "o estado e,%s\n", estado);
 
 printf("Digite o codigo: ");
 scanf ("%s", codigo); 
-printf ( "o codigo e,%s\n", codigo);
 
 printf("Digite o nome da cidade: ");
-scanf ("%s", nomedacidade); 
-printf ( "o nome da cidade e,%s\n", nomedacidade);
+scanf ("%s", nomedacidade);
 
 printf("Digite a população: ");
-scanf ("%d", &populaçao);
-printf ( "a população e,%d\n", populaçao);
+scanf ("%d", &populacao);
 
 printf("Digite a area: ");
 scanf ("%f", &area);
-printf ( "a área e,%.2f\n", area);
 
 printf("Digite o PIB: ");
 scanf ("%f", &PIB);
-printf ( "o PIB e,%.f\n", PIB);
 
 printf("Digite o número de pontos turisticos: ");
-scanf ("%d", &númerodepontosturisticos);
-printf ( "o número de pontos turisticos e,%d\n", numerodepontosturisticos);
+scanf ("%d", &numerodepontosturisticos);
 
 printf("Digite o estado: ");
 scanf ("%s", estado2); 
-printf ( "o estado e,%s\n", estado2);
 
 printf("Digite o codigo: ");
-scanf ("%s", codigo2); 
-printf ( "o codigo e,%s\n", codigo2);
+scanf ("%s", codigo2);
 
 printf("Digite o nome da cidade: ");
 scanf ("%s", nomedacidade2); 
-printf ( "o nome da cidade e,%s\n", nomedacidade2);
 
 printf("Digite a população: ");
-scanf ("%d", &populaçao2);
-printf ( "a população e,%d\n", populaçao2);
+scanf ("%d", &populacao2);
 
 printf("Digite a area: ");
 scanf ("%f", &area2);
-printf ( "a área e,%.2f\n", area2);
 
 printf("Digite o PIB: ");
 scanf ("%f", &PIB2);
-printf ( "o PIB e,%.f\n", PIB2);
 
 printf("Digite o número de pontos turisticos: ");
 scanf ("%d", &numerodepontosturisticos2);
-printf ( "o número de pontos turisticos e,%d\n", numerodepontosturisticos2);
 
 return 0;
 
